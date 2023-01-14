bee5640ff41596631ad5c043d6577bac995b0a89
Georgia E. Whitney <81175151+rockyrelay@users.noreply.github.com>
2023-01-14 18:44:04 +0000

    first commit

---
+++ regex.js
@@ -0,0 +1,30 @@
+// REGULAR EXPRESSIONS COURSE
+
+/////////
+
+// Literals
+/* 
+Letters, numbers, and some other characters match themselves
+*/
+/a cat/.test("that's a cat"); // true
+/a cat/.test("cat ate food"); // false
+
+// Wildcard
+/*
+. matches one character.
+*/
+/./.test(">"); // true
+
+// Boundaries
+/*
+The ^ boundary matches the beginning of a string, and the $ boundary matches the end.
+*/
+/^cat/.test("a cat"); // false
+/^the/.test("the cat"); // true
+
+// Repetition
+/*
+The + operator requires the regex before it to occur one or more times.
+*/
+/c.+t/.test("caaat"); // true
+/c.+t/.test("ct"); // false
