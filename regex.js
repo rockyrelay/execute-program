6b10427bf261bc72b0f80c8ca39ad763475d0dbd
Georgia E. Whitney <81175151+rockyrelay@users.noreply.github.com>
2023-01-15 21:24:17 +0000

    continuing with course

--- regex.js
+++ regex.js
@@ -28,3 +28,29 @@ The + operator requires the regex before it to occur one or more times.
 */
 /c.+t/.test("caaat"); // true
 /c.+t/.test("ct"); // false
+
+/////
+
+//QUIZ
+/*
+Write a regex that matches any string containing "cat".
+*/
+
+var re = /cat/;
+
+// QUIZ
+/*
+Write a regex that only recognizes the string "a single phrase". If there's anything more or less than that, the regex shouldn't match.
+*/
+
+var re = /^a single phrase$/;
+
+// QUIZ
+/*
+Write a regex that recognizes words that begin and end in "t". (The "t" at the beginning and end of the word must be separate, so the regex should match "tt" but not "t".)
+*/
+
+var re = /^t.*t$/;
+
+// ^ notes must start with t, . notes additional character
+// * notes 0 or more of this character, $ notes must end in t
