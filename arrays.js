60b09f85a03fd4e4a2389e6bd96a307fa6141ac8
Georgia E. Whitney <81175151+rockyrelay@users.noreply.github.com>
2023-03-01 22:33:18 +0000

    EP arrays review

---
+++ arrays.js
@@ -0,0 +1,26 @@
+// ARRAYS 
+
+// Basics // 
+
+/*
+Arrays are sequences of values. They have a length and a specific order.
+*/
+
+
+[1, 2, 3].length;
+RESULT:
+3
+
+const array1 = ['a', 'b', 'c'];
+array1[0];
+RESULT:
+'a'
+
+const array2 = ['a', 'b', 'c'];
+array2[1] = 'x';
+array2;
+RESULT:
+['a', 'x', 'c']
+
+///
+
