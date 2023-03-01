ae82f1efb27020e7e0a23af86f083fb16b4c156a
Georgia E. Whitney <81175151+rockyrelay@users.noreply.github.com>
2023-03-01 22:35:13 +0000

    AP arrays review - stack

--- arrays.js
+++ arrays.js
@@ -22,5 +22,25 @@ array2;
 RESULT:
 ['a', 'x', 'c']
 
-///
+////
 
+// STACK //
+
+/*
+```push``` and ```pop``` let us treat arrays as stacks. They add and remove elements to and from the end of the array.
+*/
+
+const a = [1, 2, 3];
+a.push(4);
+a;
+RESULT:
+[1, 2, 3, 4]
+
+
+const b = [1, 2, 3];
+b.pop();
+b;
+RESULT:
+[1, 2]
+
+////
